62440858f7da0b7eae5a5416070e4fb4fe603884
Nahuel Garcia <nhlgrc@hotmail.com>
2025-04-14 11:43:29 -0300

    desde saico

--- src/main/java/org/ngarcia/appejb/client/Main.java
+++ src/main/java/org/ngarcia/appejb/client/Main.java
@@ -3,27 +3,34 @@ package org.ngarcia.appejb.client;
 import org.ngarcia.webapp.ejb.models.Producto;
 import org.ngarcia.webapp.ejb.service.ServiceEjbRemote;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import java.util.List;
+import javax.naming.*;
 import java.util.Properties;
 
 public class Main {
    public static void main(String[] args) {
 
       ServiceEjbRemote service = null;
+      ServiceEjbRemote service2 = null;
 
-      final Properties env = new Properties();
-      env.put(Context.INITIAL_CONTEXT_FACTORY,"org.jboss.naming.remote.client.InitialContextFactory");
-      env.put(Context.PROVIDER_URL,"http-remoting://localhost:8080");
-      env.put("jboss.naming,client.ejb.context",true);
+      //estas configuraciones se definen en \resources\jndi.properies
+      //final Properties env = new Properties();
+      //env.put(Context.INITIAL_CONTEXT_FACTORY,"org.jboss.naming.remote.client.InitialContextFactory");
+      //env.put(Context.PROVIDER_URL,"http-remoting://localhost:8080");
+      //env.put("jboss.naming,client.ejb.context",true);
       try {
-         InitialContext ctx = new InitialContext(env);
-         service = (ServiceEjbRemote) ctx.lookup("ejb:/appejb-remote/ServiceEjb!org.ngarcia.webapp.ejb.service.ServiceEjbRemote");
+         //InitialContext ctx = new InitialContext(env);
+         InitialContext ctx = new InitialContext();
+         //servicio @Stateless (siempre usa la misma instancia)
+         //service = (ServiceEjbRemote) ctx.lookup
+         // ("ejb:/appejb-remote/ServiceEjb!org.ngarcia.webapp.ejb.service.ServiceEjbRemote");
+         //servicio @Stateful (cada vez es una nueva instancia)
+         service = (ServiceEjbRemote) ctx.lookup
+                 ("ejb:/appejb-remote/ServiceEjb!org.ngarcia.webapp.ejb.service.ServiceEjbRemote?stateful");
+         service2 = (ServiceEjbRemote) ctx.lookup
+                 ("ejb:/appejb-remote/ServiceEjb!org.ngarcia.webapp.ejb.service.ServiceEjbRemote?stateful");
 
          String saludo = service.saludar("Horacio");
-         String saludo2 = service.saludar("Micaela");
+         String saludo2 = service2.saludar("Micaela");
          System.out.println(saludo);
          System.out.println(saludo2);
 
